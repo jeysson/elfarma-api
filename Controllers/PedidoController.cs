517e46a6181f0be111123f6784b8e6b298195709
Jeysson Paiva <jeysson.paiva@auakaa.com.br>
2021-03-14 20:56:15 -0400

    ajustes

--- Controllers/PedidoController.cs
+++ Controllers/PedidoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using AllDelivery.Lib;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using NetTopologySuite.Geometries;
 
@@ -26,14 +27,60 @@ namespace AllDelivery.Api.Controllers
             Mensageiro mensageiro = new Mensageiro(200, "Operação realizada com sucesso!");
             try
             {
+                var loja = pedido.Loja;
+                var fp = pedido.FormaPagamento;
+
                 _context.Database.BeginTransaction();
                 pedido.Data = DateTime.UtcNow;
                 pedido.StatusId = 1;
+                pedido.Ende = pedido.Endereco.ToString();
+                pedido.LojaId = pedido.Loja.Id;
+                pedido.Loja = null;
+                pedido.FormaPagamentoId = pedido.FormaPagamento.Id;
+                pedido.FormaPagamento = null;
+                pedido.Location = new Point(pedido.Endereco.Lat, pedido.Endereco.Longi);
+                //
+                pedido.Itens.ForEach(o =>
+                {
+                    o.ProdutoId = o.Produto.Id;
+                    o.Produto = null;
+                });
+                //
                 _context.Pedidos.Add(pedido);
                 _context.SaveChanges();
                 _context.Database.CommitTransaction();
+                //
+                pedido.FormaPagamento = fp;
+                pedido.Loja = loja;
+                //
+                mensageiro.Dados = pedido;
 
             }catch(Exception ex)
+            {
+                _context.Database.RollbackTransaction();
+                mensageiro.Codigo = 300;
+                mensageiro.Mensagem = "Falha na operação!";
+                mensageiro.Dados = new { message = ex.Message, stack = ex.StackTrace,
+                innerMessage = ex.InnerException != null? ex.InnerException.Message: null,
+                innerstack = ex.InnerException != null ? ex.InnerException.StackTrace: null
+                };
+            }
+
+            return Ok(mensageiro);
+        }
+
+        [HttpGet("obter")]
+        public IActionResult Obter(uint idpedido)
+        {
+            Mensageiro mensageiro = new Mensageiro(200, "Operação realizada com sucesso!");
+            try
+            {                
+                mensageiro.Dados = _context.Pedidos.Include(p=> p.Itens).ThenInclude(p=> p.Produto)
+                                                    .Include(p => p.FormaPagamento)
+                                                    .Include(p => p.Loja)
+                                                    .FirstOrDefault(p=> p.Id == idpedido);
+            }
+            catch (Exception ex)
             {
                 _context.Database.RollbackTransaction();
                 mensageiro.Codigo = 300;
@@ -42,7 +89,5 @@ namespace AllDelivery.Api.Controllers
 
             return Ok(mensageiro);
         }
-
-        
     }
 }
